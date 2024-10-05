aa2f7c6705925693458a1300b232470310b130fe
deenu <akashraja1108@gmail.com>
2024-10-06 01:03:05 +0530

    Update vercel config

--- server/index.js
+++ server/index.js
@@ -9,7 +9,13 @@ const courseRouter = require("./routes/courseRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_ORIGIN,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
